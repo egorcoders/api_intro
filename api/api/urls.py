d75324499a9a8a83143088f7f50507e2ffc623ef
Egor Boev <egorcoders@gmail.com>
2022-09-16 13:18:55 +0500

    #3 done

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from api_app.views import WomenAPIView
+# from api_app.views import WomenAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/women_list/', WomenAPIView.as_view()),
+    # path('api/v1/women_list/', WomenAPIView.as_view()),
 ]
--- api/api_app/admin.py
+++ api/api_app/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
 from .models import Women, Category
+from api.settings import DEFAULT_NUMBER
 
 
+@admin.register(Women)
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content')
 
 
-admin.site.register(Women)
 admin.site.register(Category)
