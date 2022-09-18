94dfece57d7efe691ce4855ded22c78e289fe755
Egor Boev <egorcoders@gmail.com>
2022-09-18 21:09:12 +0500

    #8 set auto-merge for WomenAPIView

--- api/api_app/views.py
+++ api/api_app/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from api.api_app.models import Women
 from .serializers import WomenSerializer
 
- # Create your views here.
+
 class WomenAPIView(generics.ListAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
